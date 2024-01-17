48d20a23d0ae6350a83ced073fbb80131e7e5419
ErikOkau <erikmsorheim@yahoo.no>
2024-01-17 12:19:50 +0100

    Removed .ouput/build of application

--- .output/server/chunks/app/_nuxt/index-styles.9a8db109.mjs
+++
@@ -1,8 +0,0 @@
-const threeJS_vue_vue_type_style_index_0_scoped_1cf8a014_lang = ".position[data-v-1cf8a014]{cursor:pointer}";
-
-const index_vue_vue_type_style_index_0_scoped_249c74c6_lang = ".header[data-v-249c74c6]{margin-left:6.4rem;margin-top:5rem}.header h1[data-v-249c74c6]{font-size:3rem;font-weight:300}.header h2[data-v-249c74c6]{font-size:1.5rem;font-weight:300;margin-top:1rem;width:31rem}.threeJS[data-v-249c74c6]{margin-left:60%;margin-top:-22rem}";
-
-const indexStyles_9a8db109 = [threeJS_vue_vue_type_style_index_0_scoped_1cf8a014_lang, index_vue_vue_type_style_index_0_scoped_249c74c6_lang, index_vue_vue_type_style_index_0_scoped_249c74c6_lang];
-
-export { indexStyles_9a8db109 as default };
-//# sourceMappingURL=index-styles.9a8db109.mjs.map
